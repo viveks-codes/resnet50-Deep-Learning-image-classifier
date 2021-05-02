4fbfabeebf3ec9904ea2fa2a433d89fb7f03968c
vivek patel <55041874+vivolscute@users.noreply.github.com>
2021-05-02 09:26:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ model.make_predict_function()          # Necessary
 
 # You can also use pretrained model from Keras
 # Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('models/model_resnet.h5')
+from keras.applications.resnet50 import ResNet50
+model = ResNet50(weights='imagenet')
+model.save('models/model_resnet.h5')
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
